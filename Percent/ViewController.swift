38034f6914f608867de7975f39ce17f9e381772f
Junkyu Jeon <junq.jeon@gmail.com>
2018-12-17 15:01:37 +0900

    Initial Commit

---
+++ Percent/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Percent
+//
+//  Created by Jun-kyu Jeon on 17/12/2018.
+//  Copyright © 2018 연애대장. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
