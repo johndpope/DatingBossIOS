72cab50502a368133bd08174956ea40dd3894fb3
Junkyu Jeon <junq.jeon@gmail.com>
2019-01-01 22:15:41 +0900

    Main-Recommends

---
+++ Percent/UserInterface/Recommends/RecommendsCollectionViewCell.swift
@@ -0,0 +1,205 @@
+//
+//  RecommendsCollectionViewCell.swift
+//  Percent
+//
+//  Created by Jun-kyu Jeon on 24/11/2018.
+//  Copyright © 2018 연애대장. All rights reserved.
+//
+
+import UIKit
+
+import PINRemoteImage
+
+class RecommendsCollectionViewCell: UICollectionViewCell {
+    class var sectionInset: UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: 16 * QUtils.optimizeRatio(), bottom: 0, right: 16 * QUtils.optimizeRatio())
+    }
+    
+    class var minimumInteritemSpacing: CGFloat {
+        return 20 * QUtils.optimizeRatio()
+    }
+    
+    class var itemSize: CGSize {
+        get {
+            var size = CGSize.zero
+            size.width = ((UIScreen.main.bounds.size.width - RecommendsCollectionViewCell.sectionInset.left - RecommendsCollectionViewCell.sectionInset.right - RecommendsCollectionViewCell.minimumInteritemSpacing) / 2).rounded(.down)
+            size.height = size.width + 54 * QUtils.optimizeRatio()
+            return size
+        }
+    }
+    
+    private let imageView = UIImageView()
+    private let borderBackView = CircleIndicatorView()
+    
+    private let labelName = UILabel()
+    private let labelPercent = UILabel()
+    
+    private let labelRegion = UILabel()
+    private let labelJobTitle = UILabel()
+    private let labelAge = UILabel()
+    
+    private var percent = 0
+    
+    private var timer: Timer?
+    
+    var data: RecommendData? {
+        didSet {
+            if let memberIndex = data?.mem_idx, let imageName = data?.picture_name  {
+                imageView.pin_setImage(from: URL(string: RequestUrl.Image.File + "\(memberIndex)/\(imageName)"))
+            } else {
+                imageView.image = nil
+            }
+            
+            labelName.text = data?.nickname
+            borderBackView.value = 0
+            percent = 0
+            labelPercent.text = "0%"
+            
+            labelRegion.text = data?.area
+            labelJobTitle.text = data?.job
+            labelAge.text = data?.age != nil ? "\(data!.age)세" : nil
+            
+            timer?.invalidate()
+            timer = nil
+            
+            guard let target = data?.sim_sum else { return }
+            timer = Timer.scheduledTimer(withTimeInterval: 0.03, repeats: true, block: { (aTimer) in
+                self.percent += 1
+                guard self.percent <= target else {
+                    self.timer?.invalidate()
+                    self.timer = nil
+                    return
+                }
+                
+                self.labelPercent.text = "\(self.percent)%"
+                
+                self.layoutIfNeeded()
+                
+                self.borderBackView.value = CGFloat(self.percent) / 100
+            })
+            
+            self.layoutIfNeeded()
+        }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.backgroundColor = #colorLiteral(red: 0.9333333333, green: 0.9333333333, blue: 0.9333333333, alpha: 1)
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.layer.cornerRadius = RecommendsCollectionViewCell.itemSize.width / 2
+        self.contentView.addSubview(imageView)
+        
+        imageView.centerXAnchor.constraint(equalTo: self.contentView.centerXAnchor).isActive = true
+        imageView.topAnchor.constraint(equalTo: self.contentView.topAnchor).isActive = true
+        imageView.widthAnchor.constraint(equalToConstant: imageView.layer.cornerRadius * 2).isActive = true
+        imageView.heightAnchor.constraint(equalTo: self.contentView.widthAnchor).isActive = true
+        
+//        let borderView = CircleIndicatorView()
+//        borderView.translatesAutoresizingMaskIntoConstraints = false
+//        borderView.colour = #colorLiteral(red: 0.9333333333, green: 0.9333333333, blue: 0.9333333333, alpha: 0.2)
+//        borderView.value = 1.0
+//        self.contentView.addSubview(borderView)
+//        
+//        borderView.topAnchor.constraint(equalTo: imageView.topAnchor).isActive = true
+//        borderView.bottomAnchor.constraint(equalTo: imageView.bottomAnchor).isActive = true
+//        borderView.leadingAnchor.constraint(equalTo: imageView.leadingAnchor).isActive = true
+//        borderView.trailingAnchor.constraint(equalTo: imageView.trailingAnchor).isActive = true
+        
+        borderBackView.translatesAutoresizingMaskIntoConstraints = false
+        self.contentView.addSubview(borderBackView)
+        
+        borderBackView.topAnchor.constraint(equalTo: imageView.topAnchor).isActive = true
+        borderBackView.bottomAnchor.constraint(equalTo: imageView.bottomAnchor).isActive = true
+        borderBackView.leadingAnchor.constraint(equalTo: imageView.leadingAnchor).isActive = true
+        borderBackView.trailingAnchor.constraint(equalTo: imageView.trailingAnchor).isActive = true
+        
+        var containerView = UIView()
+        containerView.translatesAutoresizingMaskIntoConstraints = false
+        containerView.backgroundColor = .clear
+        self.contentView.addSubview(containerView)
+        
+        containerView.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 17 * QUtils.optimizeRatio()).isActive = true
+        containerView.centerXAnchor.constraint(equalTo: self.contentView.centerXAnchor).isActive = true
+        containerView.heightAnchor.constraint(equalToConstant: 20 * QUtils.optimizeRatio()).isActive = true
+        
+        labelName.translatesAutoresizingMaskIntoConstraints = false
+        labelName.textColor = #colorLiteral(red: 0.2901960784, green: 0.2901960784, blue: 0.2901960784, alpha: 1)
+        labelName.font = UIFont.systemFont(ofSize: 14 * QUtils.optimizeRatio(), weight: .bold)
+        containerView.addSubview(labelName)
+        
+        labelName.centerYAnchor.constraint(equalTo: containerView.centerYAnchor).isActive = true
+        labelName.leadingAnchor.constraint(equalTo: containerView.leadingAnchor).isActive = true
+        
+        labelPercent.translatesAutoresizingMaskIntoConstraints = false
+        labelPercent.textColor = #colorLiteral(red: 0.937254902, green: 0.2509803922, blue: 0.2941176471, alpha: 1)
+        labelPercent.font = UIFont.systemFont(ofSize: 14 * QUtils.optimizeRatio(), weight: .bold)
+        containerView.addSubview(labelPercent)
+        
+        labelPercent.centerYAnchor.constraint(equalTo: containerView.centerYAnchor).isActive = true
+        labelPercent.leadingAnchor.constraint(equalTo: labelName.trailingAnchor, constant: 6 * QUtils.optimizeRatio()).isActive = true
+        labelPercent.trailingAnchor.constraint(equalTo: containerView.trailingAnchor).isActive = true
+        
+        let bottomAnchor = containerView.bottomAnchor
+        
+        containerView = UIView()
+        containerView.translatesAutoresizingMaskIntoConstraints = false
+        containerView.backgroundColor = .clear
+        self.contentView.addSubview(containerView)
+        
+        containerView.topAnchor.constraint(equalTo: bottomAnchor).isActive = true
+        containerView.centerXAnchor.constraint(equalTo: self.contentView.centerXAnchor).isActive = true
+        containerView.heightAnchor.constraint(equalToConstant: 20 * QUtils.optimizeRatio()).isActive = true
+        
+        labelRegion.translatesAutoresizingMaskIntoConstraints = false
+        labelRegion.textColor = #colorLiteral(red: 0.4156862745, green: 0.4117647059, blue: 0.4156862745, alpha: 1)
+        labelRegion.font = UIFont.systemFont(ofSize: 14 * QUtils.optimizeRatio(), weight: .regular)
+        containerView.addSubview(labelRegion)
+        
+        labelRegion.centerYAnchor.constraint(equalTo: containerView.centerYAnchor).isActive = true
+        labelRegion.leadingAnchor.constraint(equalTo: containerView.leadingAnchor).isActive = true
+        
+        var seperator = UIView()
+        seperator.translatesAutoresizingMaskIntoConstraints = false
+        seperator.backgroundColor = #colorLiteral(red: 0.8784313725, green: 0.8745098039, blue: 0.8666666667, alpha: 1)
+        contentView.addSubview(seperator)
+        
+        seperator.topAnchor.constraint(equalTo: labelRegion.topAnchor, constant: 2).isActive = true
+        seperator.bottomAnchor.constraint(equalTo: labelRegion.bottomAnchor, constant: -2).isActive = true
+        seperator.leadingAnchor.constraint(equalTo: labelRegion.trailingAnchor, constant: 4 * QUtils.optimizeRatio()).isActive = true
+        seperator.widthAnchor.constraint(equalToConstant: 1).isActive = true
+        
+        labelJobTitle.translatesAutoresizingMaskIntoConstraints = false
+        labelJobTitle.textColor = #colorLiteral(red: 0.4156862745, green: 0.4117647059, blue: 0.4156862745, alpha: 1)
+        labelJobTitle.font = UIFont.systemFont(ofSize: 14 * QUtils.optimizeRatio(), weight: .regular)
+        containerView.addSubview(labelJobTitle)
+        
+        labelJobTitle.centerYAnchor.constraint(equalTo: containerView.centerYAnchor).isActive = true
+        labelJobTitle.leadingAnchor.constraint(equalTo: seperator.trailingAnchor, constant: 4 * QUtils.optimizeRatio()).isActive = true
+        
+        seperator = UIView()
+        seperator.translatesAutoresizingMaskIntoConstraints = false
+        seperator.backgroundColor = #colorLiteral(red: 0.8784313725, green: 0.8745098039, blue: 0.8666666667, alpha: 1)
+        contentView.addSubview(seperator)
+        
+        seperator.topAnchor.constraint(equalTo: labelJobTitle.topAnchor, constant: 2).isActive = true
+        seperator.bottomAnchor.constraint(equalTo: labelJobTitle.bottomAnchor, constant: -2).isActive = true
+        seperator.leadingAnchor.constraint(equalTo: labelJobTitle.trailingAnchor, constant: 4 * QUtils.optimizeRatio()).isActive = true
+        seperator.widthAnchor.constraint(equalToConstant: 1).isActive = true
+        
+        labelAge.translatesAutoresizingMaskIntoConstraints = false
+        labelAge.textColor = #colorLiteral(red: 0.4156862745, green: 0.4117647059, blue: 0.4156862745, alpha: 1)
+        labelAge.font = UIFont.systemFont(ofSize: 14 * QUtils.optimizeRatio(), weight: .regular)
+        containerView.addSubview(labelAge)
+        
+        labelAge.centerYAnchor.constraint(equalTo: containerView.centerYAnchor).isActive = true
+        labelAge.leadingAnchor.constraint(equalTo: seperator.trailingAnchor, constant: 4 * QUtils.optimizeRatio()).isActive = true
+        labelAge.trailingAnchor.constraint(equalTo: containerView.trailingAnchor).isActive = true
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError()
+    }
+}
---
+++ Percent/UserInterface/Recommends/RecommendsViewController.swift
@@ -0,0 +1,216 @@
+//
+//  RecommendsViewController.swift
+//  Percent
+//
+//  Created by Jun-kyu Jeon on 20/11/2018.
+//  Copyright © 2018 연애대장. All rights reserved.
+//
+
+import UIKit
+
+class RecommendsViewController: BaseViewController {
+    private let buttonProfile = ProfileButton(cornerRadius: 20)
+    private let buttonCherries = UIButton(type: .custom)
+    
+    private let buttonMore = UIButton(type: .custom)
+    private let collectionView: UICollectionView
+    
+    private let buttonBuyCherries = UIButton(type: .custom)
+    private let buttonPremium = UIButton(type: .custom)
+    private let buttonEvents = UIButton(type: .custom)
+    
+    private var collectionData = [RecommendData]()
+    
+    private let loadingQueue = DispatchQueue(label: "RecommendsViewController.loading", qos: .userInteractive, attributes: .concurrent, autoreleaseFrequency: .workItem, target: nil)
+    
+    override init(navigationViewEffect effect: UIVisualEffect? = nil) {
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.scrollDirection = .horizontal
+        flowLayout.sectionInset = RecommendsCollectionViewCell.sectionInset
+        flowLayout.minimumInteritemSpacing = RecommendsCollectionViewCell.minimumInteritemSpacing
+        flowLayout.itemSize = RecommendsCollectionViewCell.itemSize
+        collectionView = UICollectionView(frame: CGRect.zero, collectionViewLayout: flowLayout)
+        
+        super.init(navigationViewEffect: effect)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.title = "추천"
+        
+        loadNavigationItem()
+        
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "오늘의 퍼센트"
+        label.textColor = #colorLiteral(red: 0.2901960784, green: 0.2901960784, blue: 0.2901960784, alpha: 1)
+        label.font = UIFont.systemFont(ofSize: 18 * QUtils.optimizeRatio(), weight: .bold)
+        self.view.addSubview(label)
+        
+        label.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 16 * QUtils.optimizeRatio()).isActive = true
+        label.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 16 * QUtils.optimizeRatio()).isActive = true
+        
+        buttonMore.translatesAutoresizingMaskIntoConstraints = false
+        buttonMore.setTitle("추천 더 받기 +", for: .normal)
+        buttonMore.setTitleColor(#colorLiteral(red: 0.937254902, green: 0.2509803922, blue: 0.2941176471, alpha: 1), for: .normal)
+        buttonMore.setTitleColor(#colorLiteral(red: 0.6862745098, green: 0.1843137255, blue: 0.2156862745, alpha: 1), for: .highlighted)
+        buttonMore.contentEdgeInsets = UIEdgeInsets(top: 16 * QUtils.optimizeRatio(), left: 16 * QUtils.optimizeRatio(), bottom: 16 * QUtils.optimizeRatio(), right: 16 * QUtils.optimizeRatio())
+        buttonMore.titleLabel?.font = UIFont.systemFont(ofSize: 12 * QUtils.optimizeRatio(), weight: .bold)
+        buttonMore.addTarget(self, action: #selector(self.pressedButton(_:)), for: .touchUpInside)
+        self.view.addSubview(buttonMore)
+        
+        buttonMore.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor).isActive = true
+        buttonMore.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+        
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.backgroundColor = .clear
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.register(RecommendsCollectionViewCell.self, forCellWithReuseIdentifier: "RecommendsCollectionViewCell")
+        self.view.addSubview(collectionView)
+        
+        collectionView.topAnchor.constraint(equalTo: label.bottomAnchor, constant: 24 * QUtils.optimizeRatio()).isActive = true
+        collectionView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        collectionView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+        collectionView.heightAnchor.constraint(equalToConstant: RecommendsCollectionViewCell.itemSize.height).isActive = true
+        
+        buttonBuyCherries.translatesAutoresizingMaskIntoConstraints = false
+        buttonBuyCherries.clipsToBounds = true
+        buttonBuyCherries.setBackgroundImage(UIImage.withSolid(colour: #colorLiteral(red: 0.937254902, green: 0.2509803922, blue: 0.2941176471, alpha: 1)), for: .normal)
+        buttonBuyCherries.setBackgroundImage(UIImage.withSolid(colour: #colorLiteral(red: 0.6862745098, green: 0.1843137255, blue: 0.2156862745, alpha: 1)), for: .highlighted)
+        buttonBuyCherries.setImage(UIImage(named: "img_cherry")?.recolour(with: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)).resize(maxHeight: 18 * QUtils.optimizeRatio()), for: .normal)
+        buttonBuyCherries.setImage(UIImage(named: "img_cherry")?.recolour(with: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)).resize(maxHeight: 18 * QUtils.optimizeRatio()), for: .highlighted)
+        buttonBuyCherries.setTitle("체리 구매", for: .normal)
+        buttonBuyCherries.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+        buttonBuyCherries.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .highlighted)
+        buttonBuyCherries.addTarget(self, action: #selector(self.pressedButton(_:)), for: .touchUpInside)
+        buttonBuyCherries.layer.cornerRadius = 24 * QUtils.optimizeRatio()
+        buttonBuyCherries.titleLabel?.font = UIFont.systemFont(ofSize: 16 * QUtils.optimizeRatio(), weight: .bold)
+        buttonBuyCherries.titleEdgeInsets = UIEdgeInsets(top: 0, left: 8 * QUtils.optimizeRatio(), bottom: 0, right: 0)
+        buttonBuyCherries.imageEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 8 * QUtils.optimizeRatio())
+        self.view.addSubview(buttonBuyCherries)
+        
+        buttonBuyCherries.topAnchor.constraint(equalTo: collectionView.bottomAnchor, constant: 14 * QUtils.optimizeRatio()).isActive = true
+        buttonBuyCherries.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 16 * QUtils.optimizeRatio()).isActive = true
+        buttonBuyCherries.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -16 * QUtils.optimizeRatio()).isActive = true
+        buttonBuyCherries.heightAnchor.constraint(equalToConstant: buttonBuyCherries.layer.cornerRadius * 2).isActive = true
+        
+        buttonPremium.translatesAutoresizingMaskIntoConstraints = false
+        buttonPremium.clipsToBounds = true
+        buttonPremium.setBackgroundImage(UIImage.withSolid(colour: #colorLiteral(red: 0.6862745098, green: 0.1843137255, blue: 0.2156862745, alpha: 1)), for: .normal)
+        buttonPremium.setBackgroundImage(UIImage.withSolid(colour: #colorLiteral(red: 0.937254902, green: 0.2509803922, blue: 0.2941176471, alpha: 1)), for: .highlighted)
+        buttonPremium.setTitle("프리미엄 서비스", for: .normal)
+        buttonPremium.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+        buttonPremium.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .highlighted)
+        buttonPremium.addTarget(self, action: #selector(self.pressedButton(_:)), for: .touchUpInside)
+        buttonPremium.layer.cornerRadius = 24 * QUtils.optimizeRatio()
+        buttonPremium.titleLabel?.font = UIFont.systemFont(ofSize: 16 * QUtils.optimizeRatio(), weight: .bold)
+        self.view.addSubview(buttonPremium)
+        
+        buttonPremium.topAnchor.constraint(equalTo: buttonBuyCherries.bottomAnchor, constant: 8 * QUtils.optimizeRatio()).isActive = true
+        buttonPremium.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 16 * QUtils.optimizeRatio()).isActive = true
+        buttonPremium.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.centerXAnchor, constant: -4 * QUtils.optimizeRatio()).isActive = true
+        buttonPremium.heightAnchor.constraint(equalToConstant: buttonPremium.layer.cornerRadius * 2).isActive = true
+        
+        buttonEvents.translatesAutoresizingMaskIntoConstraints = false
+        buttonEvents.clipsToBounds = true
+        buttonEvents.setBackgroundImage(UIImage.withSolid(colour: #colorLiteral(red: 0.9764705882, green: 0.3921568627, blue: 0.4352941176, alpha: 1)), for: .normal)
+        buttonEvents.setBackgroundImage(UIImage.withSolid(colour: #colorLiteral(red: 0.6862745098, green: 0.1843137255, blue: 0.2156862745, alpha: 1)), for: .highlighted)
+        buttonEvents.setTitle("이벤트", for: .normal)
+        buttonEvents.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+        buttonEvents.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .highlighted)
+        buttonEvents.addTarget(self, action: #selector(self.pressedButton(_:)), for: .touchUpInside)
+        buttonEvents.layer.cornerRadius = buttonPremium.layer.cornerRadius
+        buttonEvents.titleLabel?.font = UIFont.systemFont(ofSize: 16 * QUtils.optimizeRatio(), weight: .bold)
+        self.view.addSubview(buttonEvents)
+        
+        buttonEvents.centerYAnchor.constraint(equalTo: buttonPremium.centerYAnchor).isActive = true
+        buttonEvents.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.centerXAnchor, constant: 4 * QUtils.optimizeRatio()).isActive = true
+        buttonEvents.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -16 * QUtils.optimizeRatio()).isActive = true
+        buttonEvents.heightAnchor.constraint(equalTo: buttonPremium.heightAnchor).isActive = true
+        
+        self.view.layoutIfNeeded()
+        
+        reloadData()
+    }
+    
+    override func pressedButton(_ sender: UIButton) {
+        super.pressedButton(sender)
+        
+        switch sender {
+        case buttonProfile:
+            break
+        default:
+            break
+        }
+    }
+    
+    func loadNavigationItem() {
+        buttonProfile.member_idx = MyData.shared.mem_idx
+        buttonProfile.imageName = MyData.shared.picture_name
+        buttonProfile.translatesAutoresizingMaskIntoConstraints = false
+        buttonProfile.addTarget(self, action: #selector(self.pressedButton(_:)), for: .touchUpInside)
+        self.navigationView.contentView.addSubview(buttonProfile)
+        
+        buttonProfile.reloadData()
+        
+        buttonProfile.bottomAnchor.constraint(equalTo: self.navigationView.contentView.bottomAnchor).isActive = true
+        buttonProfile.leadingAnchor.constraint(equalTo: self.navigationView.contentView.leadingAnchor, constant: 6 * QUtils.optimizeRatio()).isActive = true
+        buttonProfile.widthAnchor.constraint(equalToConstant: 60 * QUtils.optimizeRatio()).isActive = true
+        buttonProfile.heightAnchor.constraint(equalToConstant: kHeightNavigationView).isActive = true
+    }
+    
+    private func reloadData() {
+        loadingQueue.sync {
+            self.collectionView.reloadData()
+            
+            let httpClient = QHttpClient()
+            httpClient.request(to: RequestUrl.Main.Recommends + "\(MyData.shared.mem_idx)", method: .get, params: nil) { (isSucceed, errMessage, response) in
+                guard let responseData = response as? [[String:Any]] else { return }
+                
+                self.collectionData.append(contentsOf: responseData.map({ (item) -> RecommendData in
+                    return RecommendData(with: item)
+                }))
+                self.collectionView.reloadData()
+            }
+        }
+    }
+}
+
+extension RecommendsViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let data = collectionData[indexPath.row]
+        
+        guard let mem_idx = data.mem_idx else { return }
+        
+        LoadingIndicatorManager.shared.showIndicatorView()
+        
+        let userData = UserData()
+        userData.mem_idx = mem_idx
+        userData.reloadData { (isSucceed) in
+            LoadingIndicatorManager.shared.hideIndicatorView()
+            
+            let viewController = UserProfileViewController(data: userData)
+            self.navigationController?.pushViewController(viewController, animated: true)
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return collectionData.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return collectionView.dequeueReusableCell(withReuseIdentifier: "RecommendsCollectionViewCell", for: indexPath) as? RecommendsCollectionViewCell ?? UICollectionViewCell()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        guard let theCell = cell as? RecommendsCollectionViewCell else { return }
+        
+        theCell.data = collectionData[indexPath.row]
+    }
+}
