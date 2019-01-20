28f0c18fd4cc42fb87f2588ab2a04613691836f1
Junkyu Jeon <junq.jeon@gmail.com>
2019-01-20 11:46:31 +0900

    지인. 만나지 않기

---
+++ Percent/UserInterface/Settings/AvoidKnownsContactsViewController.swift
@@ -0,0 +1,11 @@
+//
+//  AvoidKnownsContactsViewController.swift
+//  Percent
+//
+//  Created by Jun-kyu Jeon on 20/01/2019.
+//  Copyright © 2019 연애대장. All rights reserved.
+//
+
+import UIKit
+
+import Contacts
