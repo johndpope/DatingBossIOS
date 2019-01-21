314d7d91afd743e1e125981f84d85eb107a7ef5e
Junkyu Jeon <junq.jeon@gmail.com>
2019-01-21 14:34:56 +0900

    commit

--- Percent/UserInterface/Category/CategoryViewController.swift
+++ Percent/UserInterface/Category/CategoryViewController.swift
@@ -65,7 +65,7 @@ class CategoryViewController: BaseMainViewController {
 
 extension CategoryViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let requesting = {() -> Void in
+        let requesting = {(free: Bool) -> Void in
             guard indexPath.row < self.tableData.count else { return }
             
             LoadingIndicatorManager.shared.showIndicatorView()
@@ -77,12 +77,15 @@ extension CategoryViewController: UITableViewDelegate, UITableViewDataSource {
             
             let httpClient = QHttpClient()
             httpClient.request(to: RequestUrl.Category.GetList + "\(MyData.shared.mem_idx)", params: params) { (isSucceed, errMessage, response) in
+                LoadingIndicatorManager.shared.hideIndicatorView()
                 guard isSucceed, let responseData = response as? [String:Any], let mem_idx = responseData["mem_idx"] as? Int else {
-                    LoadingIndicatorManager.shared.hideIndicatorView()
-                    
-                    let alertController = UIAlertController(title: "카테고리", message: "추천 상대를 찾을 수 없습니다.", preferredStyle: .alert)
-                    alertController.addAction(UIAlertAction(title: kStringConfirm, style: .cancel, handler: nil))
-                    self.present(alertController, animated: true, completion: nil)
+                    let alertController = AlertPopupViewController(withTitle: "카테고리 결과", message: "카테고리에 맞는 회원이 없습니다.\n다른 카테고리를 선택하세요." + (free ? "" : "\n(체리 차감 안됨)"))
+                    alertController.titleColour = #colorLiteral(red: 0.937254902, green: 0.2509803922, blue: 0.2941176471, alpha: 1)
+                    alertController.messageColour = #colorLiteral(red: 0.1333333333, green: 0.1333333333, blue: 0.1333333333, alpha: 1)
+                    alertController.addAction(action: AlertPopupAction(backgroundColour: #colorLiteral(red: 0.8, green: 0.8, blue: 0.8, alpha: 1), title: "확인", colour: #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1), font: UIFont.systemFont(ofSize: 14 * QUtils.optimizeRatio(), weight: .bold), completion: nil))
+                    UIApplication.appDelegate().window?.addSubview(alertController.view)
+                    self.addChild(alertController)
+                    alertController.show()
                     return
                 }
                 
@@ -91,30 +94,45 @@ extension CategoryViewController: UITableViewDelegate, UITableViewDataSource {
                     NotificationCenter.default.post(name: NotificationName.Cherry.Changed, object: cherry_quantity)
                 }
                 
-                let userData = UserData()
-                userData.mem_idx = mem_idx
-                userData.reloadData { (isSucceed) in
-                    LoadingIndicatorManager.shared.hideIndicatorView()
+                let alertController = AlertPopupViewController(withTitle: "카테고리 결과", message: "카테고리에 맞는 회원을 찾았습니다." + (free ? "" : "\n체리 1개가 차감되었습니다."))
+                alertController.titleColour = #colorLiteral(red: 0.937254902, green: 0.2509803922, blue: 0.2941176471, alpha: 1)
+                alertController.messageColour = #colorLiteral(red: 0.1333333333, green: 0.1333333333, blue: 0.1333333333, alpha: 1)
+                alertController.addAction(action: AlertPopupAction(backgroundColour: #colorLiteral(red: 0.8, green: 0.8, blue: 0.8, alpha: 1), title: "확인", colour: #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1), font: UIFont.systemFont(ofSize: 14 * QUtils.optimizeRatio(), weight: .bold), completion: {(action) -> Void in
+                    LoadingIndicatorManager.shared.showIndicatorView()
                     
-                    let viewController = UserProfileViewController(data: userData)
-                    self.navigationController?.pushViewController(viewController, animated: true)
-                }
+                    let userData = UserData()
+                    userData.mem_idx = mem_idx
+                    userData.reloadData { (isSucceed) in
+                        LoadingIndicatorManager.shared.hideIndicatorView()
+                        
+                        let viewController = UserProfileViewController(data: userData)
+                        self.navigationController?.pushViewController(viewController, animated: true)
+                    }
+                }))
+                UIApplication.appDelegate().window?.addSubview(alertController.view)
+                self.addChild(alertController)
+                alertController.show()
             }
         }
         
         let httpClient = QHttpClient()
         httpClient.request(to: RequestUrl.Service.Free + "\(MyData.shared.mem_idx)", method: .get, headerValues: nil, params: nil) { (isSucceed, errMessage, response) in
-            guard let responseData = response as? [String:Any], (responseData["category"] as? Int ?? 0) > 0 else {
-                requesting()
-                return
+            var titleString = "유료 카테고리"
+            var message = "체리 1개를 사용하여 확인하시겠습니까?\n내가 가진 체리 \(MyData.shared.cherry_quantity)개"
+            var isFree = false
+            
+            if let responseData = response as? [String:Any], (responseData["category"] as? Int ?? 0) > 0 {
+                titleString = "무료 카테고리"
+                message = "1일 1회 무료 카테고리를 사용합니다."
+                isFree = true
             }
             
-            let alertController = AlertPopupViewController(withTitle: "무료 카테고리", message: "1일 1뢰 무료 카테고리를 사용합니다.")
+            let alertController = AlertPopupViewController(withTitle: titleString, message: message)
             alertController.titleColour = #colorLiteral(red: 0.937254902, green: 0.2509803922, blue: 0.2941176471, alpha: 1)
             alertController.messageColour = #colorLiteral(red: 0.1333333333, green: 0.1333333333, blue: 0.1333333333, alpha: 1)
             alertController.addAction(action: AlertPopupAction(backgroundColour: #colorLiteral(red: 0.8, green: 0.8, blue: 0.8, alpha: 1), title: "취소", colour: #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1), font: UIFont.systemFont(ofSize: 14 * QUtils.optimizeRatio(), weight: .bold), completion: nil))
             alertController.addAction(action: AlertPopupAction(backgroundColour: #colorLiteral(red: 0.937254902, green: 0.2509803922, blue: 0.2941176471, alpha: 1), title: "확인", colour: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), font: UIFont.systemFont(ofSize: 14 * QUtils.optimizeRatio(), weight: .bold), completion: { (action) in
-                requesting()
+                requesting(isFree)
             }))
             UIApplication.appDelegate().window?.addSubview(alertController.view)
             self.addChild(alertController)
